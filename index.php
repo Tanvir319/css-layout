5712cf3e09def73d3858ef666d9edf84b37b3707
Tanvir319 <75492071+Tanvir319@users.noreply.github.com>
2021-04-04 10:45:31 +0600

    css layout linking

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>CSS layout</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="/css/style.css" media="screen"/>
+</head>
+<body>
+<header>
+  <h2>Hello welcome to our page</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="#">Dhaka</a></li>
+      <li><a href="#">Sirajganj</a></li>
+      <li><a href="#">Bogura</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>Dhaka</h1>
+    <p>Dhaka is the capital city of Bangladesh. It is the most populous city in Bangladesh.</p>
+  </article>
+</section>
+
+<footer>
+  <p>©2021</p>
+</footer>
+
+</body>
+</html>
